97c1911dd0b18aee7e3d74827473ad1894be77f2
Mike Welte <mwelte1@binghamton.edu>
2013-10-25 13:50:13 -0400

    Started work on the driver and parser and fixed the build.xml file

---
+++ src/reflection/util/Parse.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+public class Parse 
+{
+	BufferedReader inputFile;
+
+	public Parse(String filename) throws FileNotFoundException
+	{
+		try
+		{
+			//Opens the file
+			inputFile = new BufferedReader(new FileReader(filename));
+		}
+		catch(FileNotFoundException e)
+		{
+			throw new FileNotFoundException("ERROR: File not found!" + e.toString());
+		}		
+	}
+
+	public Boolean parseAll()
+	{
+		
+		return true;
+	}
+
+	/**
+	*	This method gets the next object from the 
+	*	 text file and returns the object as a String
+	*	 array.
+	*	@return Returns a String array with a object
+	*	 as defined in the XML format (unparsed raw text)
+	**/
+	public String[] getObjectArray()
+	{
+		//Read <DPSerialization> 
+		//Read the object type and path
+		//Read all variables for object and initalize
+		//read </complexType>
+		//read </DPSerialization>
+
+		//put "good input" into array, just ignore bad imput
+
+		//increment counter for number of objects
+
+		return null;
+
+	}
+}
