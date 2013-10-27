b970103fb1786ca690601987cbdc23106e7faa0f
Mike <mike@Lysander.(none)>
2013-10-26 23:43:01 -0400

    Added the package name for the parse class

--- src/reflection/util/Parse.java
+++ src/reflection/util/Parse.java
@@ -1,4 +1,6 @@
-
+//---------------------------------------------------------------------
+package reflection.util;
+//---------------------------------------------------------------------
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
